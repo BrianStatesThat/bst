5ce10bc579e82e2421e4eabbf298a80f7f2abd95
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2025-01-19 08:50:14 +0000

    Preparing for dark mode

--- brianstatesthat/components/Navbar.jsx
+++ brianstatesthat/components/Navbar.jsx
@@ -21,6 +21,10 @@ const Navbar = () => {
 
   return (
     <>
+        <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+            <Image src={assets.header_bg_color} alt='' className='w-full' />
+        </div>
+
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
         flex items-center justify-between z-50'>
             <a href="#top">
@@ -90,4 +94,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
