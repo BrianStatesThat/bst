dd7da6b877d4845806c986b8ee99dbe12bcda75c
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2025-01-26 20:41:16 +0000

    1.51

---
+++ brianstatesthat/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact'>
+        <h4 className='text-center mb-2 text-lg font-Ovo'>Portfolio</h4>
+        <h2 className='text-center text-5xl font-Ovo'>Latest work</h2>
+
+        <p className='text-center max-w-2x mx-auto mt-5 mb-12 font-Ovo'>
+            Welcome to my portfolio! Explore my journey in web development and AI prompting. Discover projects showcasing my expertise in front-end development.
+        </p>
+    </div>
+  )
+}
+
+export default Contact
--- brianstatesthat/components/Work.jsx
+++ brianstatesthat/components/Work.jsx
@@ -16,18 +16,29 @@ const Work = () => {
             {workData.map((project, index)=>(
                 <div className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
                  key={index} style={{backgroundImage: `url(${project.bgImage})`}}>
-                    <div>
+                    <div className='bg-white w-10/12 rounded-md absolute bottom-5
+                    left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500
+                    group-hover:bottom-7'>
                         <div>
-                            <h2>{project.title}</h2>
-                            <p>{project.description}</p>
+                            <h2 className='font-semibold'>{project.title}</h2>
+                            <p className='text-sm text-gray-700'>{project.description}</p>
                         </div>
-                        <div>
+                        <div className='border rounded-full border-black w-9 aspect-square flex
+                        items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition
+                        transition'>
                             <Image src={assets.send_icon} alt='send icon' className='w-5' />
                         </div>
                     </div>
                 </div>
             ))}
         </div>
+
+        <a href="" className='w-max flex itmes-center justify-center gap-2 text-gray-700
+        border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500'>
+            Show more<Image src={assets.right_arrow_bold} alt='right arrow'
+            className='w-4 h-4 py-auto my-auto' />
+        </ a>
+
     </div>
   )
 }
