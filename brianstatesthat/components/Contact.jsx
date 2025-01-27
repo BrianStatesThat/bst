0a795385c3ed1603e9e834f06e01b3c0d395cfc0
Brian Speelman <67787940+BrianStatesThat@users.noreply.github.com>
2025-01-27 10:42:35 +0000

    Addition of details to the contact form

--- brianstatesthat/components/Contact.jsx
+++ brianstatesthat/components/Contact.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id='contact'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>Portfolio</h4>
-        <h2 className='text-center text-5xl font-Ovo'>Latest work</h2>
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat
+    bg-center bg-[length:90%_auto]'>
+        <h4 className='text-center mb-2 text-lg font-Ovo'>Let's Connect</h4>
+        <h2 className='text-center text-5xl font-Ovo'>Send me a message</h2>
 
         <p className='text-center max-w-2x mx-auto mt-5 mb-12 font-Ovo'>
-            Welcome to my portfolio! Explore my journey in web development and AI prompting. Discover projects showcasing my expertise in front-end development.
+        Share your name, email, and a little about your project or question.
+         I would love to hear from you! Once you are ready, click Submit, and I will be in touch soon to help however I can. Looking forward to connecting!
         </p>
     </div>
   )
